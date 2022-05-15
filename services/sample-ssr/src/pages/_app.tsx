ecae068d2738554bf9b17c47590675ae6fd3de8a
sckimynwa <tsb03672@naver.com>
2022-05-15 12:53:04 +0900

    feat: update title

--- services/sample-ssr/src/pages/_app.tsx
+++ services/sample-ssr/src/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
         />
-        <title>SAMPLE SSR</title>
+        <title>YeoulCoding SSR</title>
       </Head>
       <Component {...pageProps} />
     </>
