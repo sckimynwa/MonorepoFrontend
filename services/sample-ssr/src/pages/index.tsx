8e7fc471eff17d5cc782283c8af386439e54f613
sckimynwa <tsb03672@naver.com>
2022-05-15 12:44:42 +0900

    chore: temp

--- services/sample-ssr/src/pages/index.tsx
+++ services/sample-ssr/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
-const IndexPage: NextPage = () => {
+const Index: NextPage = () => {
   return <div>This is SSR Page</div>;
 };
 
-export default IndexPage;
+export default Index;
