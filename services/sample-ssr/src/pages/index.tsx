862017718e18cf24c2ae858ad57b93f0840c698f
SangChul Kim <30401400+sckimynwa@users.noreply.github.com>
2022-05-15 20:10:25 +0900

    fix: ssr deployment (#4)
    * chore: trigger ssr
    
    * feat: update dispatch
    
    * chore: temp
    
    * chore: temp
    
    * fix: build settings
    
    * feat: update title
    
    * feat: temp
    
    * feat: update ssr
    
    * feat: build

--- services/sample-ssr/src/pages/index.tsx
+++ services/sample-ssr/src/pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next';
 
-
-const MembershipPage: NextPage = () => {
-  return (
-    <div>This is SSR Page</div>
-  );
+const IndexPage: NextPage = () => {
+  return <div>This is SSR Page</div>;
 };
 
-
-export default MembershipPage;
+export default IndexPage;
