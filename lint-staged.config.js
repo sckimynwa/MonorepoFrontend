d3235642f4f6f45c4d3e4a1792982be2ec1656e3
sckimynwa <tsb03672@naver.com>
2022-05-15 11:38:07 +0900

    feat: remove lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const escape = require('shell-quote').quote;
+
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': filenames => {
+    return [];
+    // const escapedFileNames = filenames
+    //   .map(filename => `"${escape([filename])}"`)
+    //   .join(' ');
+    // return [
+    //   `prettier --write ${escapedFileNames}`,
+    //   `eslint --fix --max-warnings=0`,
+    // ];
+  },
+  '**/*.{json,md,mdx,css,html,yml,yaml,scss}': filenames => {
+    // const escapedFileNames = filenames
+    //   .map(filename => `"${escape([filename])}"`)
+    //   .join(' ');
+    // return [`prettier --write ${escapedFileNames}`];
+    return [];
+  },
+};
